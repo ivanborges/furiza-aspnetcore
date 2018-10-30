91b8f38408babca38aeb5f0ab3c23ab8feafdda8
Ivan Borges <ivanborges@live.com>
2018-10-30 18:39:08 -0300

    Início da adaptação do projeto Identity para o Furiza.Identity.Abstractions 2.0.

--- src/Furiza.AspNetCore.Identity.EntityFrameworkCore/ApplicationUser.cs
+++ src/Furiza.AspNetCore.Identity.EntityFrameworkCore/ApplicationUser.cs
@@ -3,16 +3,31 @@ using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 
 namespace Furiza.AspNetCore.Identity.EntityFrameworkCore
 {
-    public class ApplicationUser : IdentityUser<Guid>, IUserData
+    public class ApplicationUser : IdentityUser<Guid>, IUserPrincipal
     {
         public virtual string FullName { get; set; }
+        public virtual string HiringType { get; set; }
         public virtual string Company { get; set; }
         public virtual string Department { get; set; }
 
-        public virtual ICollection<IRoleData> Roles
+        public virtual ICollection<Claim> Claims
+        {
+            get
+            {
+                return IdentityClaims?.Select(ic => ic.ToClaim()).ToList();
+            }
+            set
+            {
+                IdentityClaims = value.Select(c => new ApplicationUserClaim() { ClaimType = c.Type, ClaimValue = c.Value }).ToList();
+            }
+        }
+        public virtual ICollection<ApplicationUserClaim> IdentityClaims { get; set; }
+
+        public virtual ICollection<IRoleAssignment> RoleAssignments
         {
             get
             {
@@ -24,19 +39,6 @@ namespace Furiza.AspNetCore.Identity.EntityFrameworkCore
             }
         }
         public virtual ICollection<ApplicationUserRole> IdentityUserRoles { get; set; }
-
-        public virtual ICollection<IClaimData> Claims
-        {
-            get
-            {
-                return IdentityClaims?.Cast<IClaimData>().ToList();
-            }
-            set
-            {
-                IdentityClaims = value.Select(c => new ApplicationUserClaim() { ClaimType = c.Type, ClaimValue = c.Value }).ToList();
-            }
-        }
-        public virtual ICollection<ApplicationUserClaim> IdentityClaims { get; set; }
 
         public virtual DateTime? CreationDate { get; set; }
         public virtual string CreationUser { get; set; }
--- src/Furiza.AspNetCore.Identity.EntityFrameworkCore/ApplicationUserRole.cs
+++ src/Furiza.AspNetCore.Identity.EntityFrameworkCore/ApplicationUserRole.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Furiza.Base.Core.Identity.Abstractions;
+using Microsoft.AspNetCore.Identity;
 using System;
 
 namespace Furiza.AspNetCore.Identity.EntityFrameworkCore
 {
-    public class ApplicationUserRole : IdentityUserRole<Guid>
+    public class ApplicationUserRole : IdentityUserRole<Guid>, IRoleAssignment
     {
+        public virtual Guid ClientId { get; set; }
+
+        public virtual string UserName {
+            get
+            {
+                return IdentityUser?.UserName;
+            }
+            set
+            {
+                IdentityUser = new ApplicationUser() { UserName = value };
+            }
+        }
         public virtual ApplicationUser IdentityUser { get; set; }
+
+        public virtual string Role
+        {
+            get
+            {
+                return IdentityRole?.Name;
+            }
+            set
+            {
+                IdentityRole = new ApplicationRole() { Name = value };
+            }
+        }
         public virtual ApplicationRole IdentityRole { get; set; }
+
         public virtual DateTime? CreationDate { get; set; }
         public virtual string CreationUser { get; set; }
     }
