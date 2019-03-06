5437289a2f2627a45813ab20b43a889cf327c3ed
Ivan Borges <ivanborges@live.com>
2019-03-06 17:49:05 -0300

    Change and reset user password api methods.

---
+++ src/Furiza.AspNetCore.WebApiConfiguration.SecurityProvider/Dtos/v1/Users/ChangePasswordUsersPost.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Furiza.AspNetCore.WebApiConfiguration.SecurityProvider.Dtos.v1.Users
+{
+    public class ChangePasswordUsersPost
+    {
+        [Required]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
