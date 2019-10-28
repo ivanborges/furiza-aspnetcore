89e05425062ef0d7b29e67b957984282b929732e
Ivan Borges <ivanborges@live.com>
2019-10-28 14:41:05 -0300

    Validação à toa para o hacktoberfest 2019.

--- src/Furiza.AspNetCore.WebApi.Configuration.SecurityProvider/SecurityRootStartup.cs
+++ src/Furiza.AspNetCore.WebApi.Configuration.SecurityProvider/SecurityRootStartup.cs
@@ -12,6 +12,11 @@ namespace Furiza.AspNetCore.WebApi.Configuration.SecurityProvider
         protected SecurityRootStartup(IConfiguration configuration) : base(configuration)
         {
             AutomapperAssemblies.Add(typeof(SecurityRootStartup).Assembly);
+
+            //TODO: remover
+            var atoa = "";
+            if (!string.IsNullOrWhiteSpace(atoa))
+                throw new System.InvalidOperationException(atoa);
         }
 
         protected override void AddCustomServicesAtTheBeginning(IServiceCollection services)
